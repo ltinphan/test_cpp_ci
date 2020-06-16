f1fe3babc078a2711fe9ac4aa305fd56780c9e3e
luongtphan <luong.phan@rapyuta-robotics.com>
2020-06-16 17:24:36 +0900

    travis goooo

---
+++ test.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+
+#include <iostream> 
+
+void test(){
+	assert(1+1==2);
+}
+
+int main() {
+	#ifndef NDEBUG
+	std::cout << "Debug mode\n";
+
+
+	test(); 
+	#else
+	assert(1==2);//faslse but must be removed in release mode
+	std::cout << "Release mode\n";
+
+	#endif
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
